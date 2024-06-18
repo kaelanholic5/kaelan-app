f2c216ac1441bf5bbfe73d76b94eff2b8f1d1997
Kaelan Holic <kaelanholic@Kaelans-MacBook-Pro.local>
2024-06-18 15:32:00 -0400

    add changes

--- app/services/general/ErrorService.ts
+++
@@ -1,6 +0,0 @@
-export function handleError(error: any, errorMessage: string) {
-    if (error instanceof Error) {
-        return error.message;
-    }
-    return errorMessage;
-}
--- app/services/general/SecurityService.ts
+++
@@ -1,9 +0,0 @@
-import { Request } from 'express';
-import 'dotenv/config';
-
-export default function (request: Request) {
-    if (request?.headers?.authorization == process?.env?.SECURITY_KEY) {
-        return true;
-    }
-    return false;
-}
